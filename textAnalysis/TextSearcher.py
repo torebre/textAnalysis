e647907cfd22191e3ece6160a3ab362e18730145
Tore Brede <tore.brede@gmail.com>
2020-08-15 11:30:57 +0200

    Looking at highlighting best fragments

--- textAnalysis/TextSearcher.py
+++ textAnalysis/TextSearcher.py
@@ -12,36 +12,21 @@ class TextSearcher:
         directory = SimpleFSDirectory(Paths.get(fs_directory))
         self.searcher = IndexSearcher(DirectoryReader.open(directory))
         self.analyzer = StandardAnalyzer()
-        # run(searcher, analyzer)
 
     def search(self, searchtext):
         if searchtext is None:
             return 0
 
         query = QueryParser("contents", self.analyzer).parse(searchtext)
-        scoreDocs = self.searcher.search(query, 50).scoreDocs
-        print("%s total matching documents." % len(scoreDocs))
-        return len(scoreDocs)
+        score_docs = self.searcher.search(query, 50).scoreDocs
+        print("%s total matching documents." % len(score_docs))
+        return len(score_docs)
 
     def find_documents(self, search_text):
         query = QueryParser("contents", self.analyzer).parse(search_text)
         hits = self.searcher.search(query, 50)
 
         return hits
-
-        # for hit in hits.scoreDocs:
-        #     document_number = hit.doc
-        #     document = self.searcher.doc(document_number)
-        #     creation_date_field = document.getField("Creation-Date")
-        #     doc_name = document.getField("doc_name")
-        #
-        #     print("Creation date: %s" % creation_date_field)
-        #
-        #     date = datetime.datetime.strptime(doc_name.stringValue(), self.DATE_FORMAT)
-        #     print("Date: %s" % date)
-        #
-        #     for field in document.getFields():
-        #         print("Field: %s. Value: %s" % (field.name(), field.stringValue()))
 
     def get_document(self, document_id):
         return self.searcher.doc(document_id)
--- textAnalysis/listTerms.py
+++ textAnalysis/listTerms.py
@@ -6,7 +6,17 @@ from org.apache.lucene.index import DirectoryReader
 from org.apache.lucene.search.spell import LuceneDictionary
 from org.apache.lucene.store import FSDirectory
 from org.apache.lucene.util import BytesRef, BytesRefIterator
-
+from org.apache.lucene.search import IndexSearcher
+from org.apache.lucene.analysis.standard import StandardAnalyzer, StandardTokenizer
+from org.apache.lucene.search.highlight import Highlighter
+from org.apache.lucene.search.highlight import Formatter
+from org.apache.lucene.search.highlight import Fragmenter
+from org.apache.lucene.search.highlight import Highlighter
+from org.apache.lucene.search.highlight import QueryScorer
+from org.apache.lucene.search.highlight import SimpleHTMLFormatter
+from org.apache.lucene.queryparser.classic import QueryParser
+from org.apache.lucene.search.highlight import SimpleSpanFragmenter
+from org.apache.lucene.search.highlight import TokenSources
 
 lucene.initVM(vmargs=['-Djava.awt.headless=true'])
 
@@ -16,7 +26,36 @@ paths_dict = util.getPaths()
 fs_directory = FSDirectory.open(Paths.get(paths_dict['fs_directory']))
 index_reader = DirectoryReader.open(fs_directory)
 lucene_dictionary = LuceneDictionary(index_reader, 'contents')
-iterator = lucene_dictionary.getEntryIterator();
+iterator = lucene_dictionary.getEntryIterator()
+analyzer = StandardAnalyzer()
+searcher = IndexSearcher(index_reader)
+formatter = SimpleHTMLFormatter()
 
+counter = 0
 for term in BytesRefIterator.cast_(iterator):
-    print('term:', term.utf8ToString())
+    term_as_string = term.utf8ToString()
+    print('term:', term_as_string)
+    query = QueryParser("contents", analyzer).parse(term_as_string)
+    hits = searcher.search(query, 100000)
+    scorer = QueryScorer(query)
+    fragmenter = SimpleSpanFragmenter(scorer, 10)
+    highlighter = Highlighter(formatter, scorer)
+    highlighter.setTextFragmenter(fragmenter)
+
+    for hit in hits.scoreDocs:
+        document = searcher.doc(hit.doc)
+
+        stream = TokenSources.getAnyTokenStream(index_reader, hit.doc, 'contents', analyzer)
+        best_fragments = highlighter.getBestFragments(stream, document.get('contents'), 10)
+
+        for fragment in best_fragments:
+            print('fragment: ', fragment)
+
+    counter += 1
+    if counter > 2:
+        break
+
+# for document_number in range(index_reader.numDocs()):
+#     document = index_reader.document(document_number)
+#     for field in document.getFields():
+#         print('field: ', field)
