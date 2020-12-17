da81160ceac83a57f0472bcf1aa4f672afe7a2cd
Tore Brede <tore.brede@gmail.com>
2020-12-17 22:18:48 +0100

    Adding view for document contents

--- dashTest/app.py
+++ dashTest/app.py
@@ -9,11 +9,12 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output, State, ALL
 from textAnalysis.TextSearcher import TextSearcher
 import datetime
 import textAnalysis.utilities as util
 import html2markdown
+import json
 
 DATE_FORMAT = '%m%d%y'
 
@@ -24,7 +25,20 @@ paths_dict = util.getPaths()
 textSearcher = TextSearcher(paths_dict['fs_directory'])
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+# TODO Check that this works
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+suppress_callback_exceptions = True
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
@@ -42,7 +56,11 @@ app.layout = html.Div(children=[
 
     dcc.Graph(
         id='occurrence-scatterplot'
-    )
+    ),
+
+    html.Br(),
+
+    html.Div(id='document_text')
 ])
 
 
@@ -64,23 +82,34 @@ def do_search(n_clicks, search_text):
         env.attachCurrentThread()
 
         hits = textSearcher.find_documents(search_text)
-        date_list = []
 
-        for hit in hits.scoreDocs:
-            document_number = hit.doc
-            document = textSearcher.get_document(document_number)
-            doc_name = document.getField("doc_name")
-            date = datetime.datetime.strptime(doc_name.stringValue(), '%m%d%y')
-            date_list.append(date)
+        if len(hits.scoreDocs) == 0:
+            data_frame = pd.DataFrame([])
+            data_frame['Date'] = []
+            data_frame['Marker'] = []
+        else:
+            date_list = []
 
-        data_frame = pd.DataFrame(date_list)
-        data_frame['Marker'] = ['1'] * len(date_list)
+            for hit in hits.scoreDocs:
+                document_number = hit.doc
+                document = textSearcher.get_document(document_number)
+                doc_name = document.getField("doc_name")
+                date = datetime.datetime.strptime(doc_name.stringValue(), '%m%d%y')
+                date_list.append(date)
 
-        highlighted_hits = textSearcher.get_highlighted_hits()
+            data_frame = pd.DataFrame(date_list)
+            data_frame['Marker'] = ['1'] * len(date_list)
 
-        for highlighted_hit in highlighted_hits:
-            for hit in highlighted_hit[1]:
-                fragment_list.append(html.Li(dcc.Markdown(html2markdown.convert(hit))))
+            highlighted_hits = textSearcher.get_highlighted_hits()
+
+            for highlighted_hit in highlighted_hits:
+                counter = 0
+                for hit in highlighted_hit[1]:
+                    fragment_list.append(
+                        html.Li(html.A(dcc.Markdown(html2markdown.convert(hit)), id={
+                            'type': 'hit_document',
+                            'index': highlighted_hit[0]})))
+                    counter += 1
 
     data_frame.columns = ['Date', 'Marker']
     scatterplot = px.scatter(data_frame, x="Date", y="Marker", range_x=['2015-01-01', '2017-12-31'])
@@ -88,6 +117,22 @@ def do_search(n_clicks, search_text):
     print(fragment_list)
 
     return scatterplot, html.Ul(fragment_list)
+
+
+@app.callback(dash.dependencies.Output('document_text', 'children'),
+              [dash.dependencies.Input({'type': 'hit_document',
+                                        'index': ALL}, 'n_clicks')])
+def show_document_text(n_clicks):
+    context = dash.callback_context
+
+    if not context.triggered:
+        return
+    else:
+        # TODO Find better way of getting the document index
+        document_index = json.loads(context.triggered[0]['prop_id'].split(".")[0])['index']
+        # TODO Find better place to call this
+        env.attachCurrentThread()
+        return html.P(textSearcher.get_document(document_index).get('contents'))
 
 
 if __name__ == '__main__':
